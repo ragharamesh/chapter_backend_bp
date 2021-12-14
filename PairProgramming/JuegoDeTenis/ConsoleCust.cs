6baf6af037caca3e586b5b1f60c1bf6cfdce19de
southindianrestaurant <southindianrestaurent@gmail.com>
2021-12-14 13:45:52 -0500

    Restructure initial commit

---
+++ PairProgramming/JuegoDeTenis/ConsoleCust.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuegoDeTenis
+{
+    public class ConsoleCust : IConsole
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public void WriteLine(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ PairProgramming/JuegoDeTenis/IConsole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuegoDeTenis
+{
+    public interface IConsole
+    {
+        void WriteLine(string msg);
+        string ReadLine();
+    }
+}
